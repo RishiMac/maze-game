00152bcc291edafe381aba45762a1ce07e2c4b85
RishiMac <87948611+RishiMac@users.noreply.github.com>
2023-08-08 02:17:51 -0400

    Created the visualization of randomly generated maze on React App

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [maze, setMaze] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/generate-maze')
+      .then((response) => response.json())
+      .then((data) => {
+        setMaze(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching maze data:', error);
+      });
+  }, []);
+
+  const getCellClass = (cellValue) => {
+    if (cellValue === 0) {
+      return 'wall-cell';
+    } else if (cellValue === 16) {
+      return 'visited-cell';
+    } else if (cellValue === 31) {
+      return 'path-cell';
+    }
+
+    const directions = ['north', 'east', 'south', 'west'];
+    const pathClasses = [];
+    directions.forEach((dir, index) => {
+      if ((cellValue & (1 << index)) !== 0) {
+        pathClasses.push(`${dir}-path`);
+      }
+    });
+
+    return pathClasses.join(' ');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header className="header">
+        <h1>Maze Runner</h1>
       </header>
+      <div className="maze-container">
+        <div className="transparent-square"></div>
+        <div className="maze">
+          {maze.map((row, rowIndex) => (
+            <div key={`row-${rowIndex}`} className="maze-row">
+              {row.map((cellValue, columnIndex) => (
+                <div
+                  key={`cell-${rowIndex}-${columnIndex}`}
+                  className={`maze-cell ${getCellClass(cellValue)}`}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- spring-boot-backend/src/main/java/com/example/mazegame/mazegame/MazeController.java
+++ spring-boot-backend/src/main/java/com/example/mazegame/mazegame/MazeController.java
@@ -1,11 +1,14 @@
 package com.example.mazegame.mazegame;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin; // Import this
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000") // Replace with the actual origin of your React app
 public class MazeController {
 
     private final MazeGenerationService mazeGenerationService;
